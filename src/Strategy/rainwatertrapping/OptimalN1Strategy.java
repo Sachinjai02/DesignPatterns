ab4a3a3530fe5f9c1304b2fcf21541ce5b428816
Sachin Jain <sachin.jain.nsit@gmail.com>
2022-10-08 20:05:49 +0530

    Implementation of Rain water trapping problem using different strategies/Algos - Stratery Design Pattern

--- src/Strategy/rainwatertrapping/OptimalN1Strategy.java
+++ src/Strategy/rainwatertrapping/OptimalN1Strategy.java
@@ -13,17 +13,16 @@ public class OptimalN1Strategy implements RainWaterTrappingSolutionStrategy {
         int totalWaterTrapped = 0, leftMax = heights[0];
         for(int i=1;i<maxIdx;++i) {
             leftMax = Math.max(heights[i], leftMax);
-            int minAdjHeight = Math.min(leftMax, heights[maxIdx]);
-            if(minAdjHeight > heights[i]) {
-                totalWaterTrapped += minAdjHeight - heights[i];
+
+            if(leftMax > heights[i]) {
+                totalWaterTrapped += leftMax - heights[i];
             }
         }
         int rightMax = heights[heights.length-1];
         for(int i=heights.length-2; i>maxIdx; --i) {
             rightMax = Math.max(rightMax, heights[i]);
-            int minAdjHeight = Math.min(rightMax, heights[maxIdx]);
-            if(minAdjHeight > heights[i]) {
-                totalWaterTrapped += minAdjHeight - heights[i];
+            if(rightMax > heights[i]) {
+                totalWaterTrapped += rightMax - heights[i];
             }
         }
         return totalWaterTrapped;
