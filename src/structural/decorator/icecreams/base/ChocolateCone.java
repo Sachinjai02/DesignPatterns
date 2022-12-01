2dfd115416a725ec7b6b3b87acd3ac4066f1de59
Sachin Jain <sachin.jain.nsit@gmail.com>
2022-12-01 11:15:22 +0530

    Decorator design pattern - changing ChocolateCone to be base as well as add on

--- src/structural/decorator/icecreams/base/ChocolateCone.java
+++ src/structural/decorator/icecreams/base/ChocolateCone.java
@@ -3,13 +3,28 @@ package structural.decorator.icecreams.base;
 import structural.decorator.icecreams.IceCreamConstituent;
 
 public class ChocolateCone implements IceCreamConstituent {
+    //Chocolate Cone acts as both base as well as addon.
+    // i.e. Chocolate Cone can be put inside any other cone/constituent
+    private IceCreamConstituent base;
+    public ChocolateCone(IceCreamConstituent base) {
+        this.base = base;
+    }
+    public ChocolateCone() {
+
+    }
     @Override
     public String getDescription() {
+        if(this.base != null) {
+            return this.base.getDescription() + " + " + "Chocolate Cone";
+        }
         return "Chocolate Cone";
     }
 
     @Override
     public Integer getCost() {
+        if(this.base != null) {
+            return this.base.getCost() + 30;
+        }
         return 30;
     }
 }
